214d9436ee0765a2ab973f67c22e5bf9202405ee
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2019-04-16 15:59:30 +0200

    clean up master version

--- SW_ajax-load-post.php
+++ SW_ajax-load-post.php
@@ -9,24 +9,16 @@ Author URI: warsztatkodu.pl
 Copyright: Sebastian Wesołowski
 */
 
-// https://premium.wpmudev.org/blog/using-ajax-with-wordpress/
-// https://www.problogdesign.com/wordpress/load-next-wordpress-posts-with-ajax/
 add_action('template_redirect', 'pbd_alp_init');
   function pbd_alp_init() {
     global $wp_query;
 
-    // Add code to index pages.
     if( is_home() || is_singular('post') ) {
-        // TODO:// Rozszerzyć o query load ajax
-
-        // Queue JS and CSS
+        
         wp_enqueue_script('pbd-alp-load-posts',plugin_dir_url( __FILE__ ) . 'assets/js/SW_ajax-load-post.js',array('jquery'),'1.0', true);
-
-        // What page are we on? And what is the pages limit?
         $paged = ( get_query_var('paged') > 1 ) ? get_query_var('paged') : 1;
         $max = $wp_query->max_num_pages;
 
-        // Add some parameters for the JS.
         wp_localize_script(
             'pbd-alp-load-posts',
             'WK__loadPostAjaxObject',
@@ -43,7 +35,5 @@ add_action('template_redirect', 'pbd_alp_init');
         );
     }
 }
-// TODO:// Rozszerzyć o query load ajax
-// include 'php-query-load-ajax.php';
 
 ?>
